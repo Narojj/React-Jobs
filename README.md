943b1dd32d14fece5140f665163e8eca292d60b3
Jaron <jaron.rolls12@gmail.com>
2025-08-13 21:47:53 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# React-Jobs
+This project uses the basics of React, such as components, props, state, data fetching, and more, while building a job listing frontend.
+
+run npm run server & npm run dev to display.
---
+++ src/components/Pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import Hero from "../Hero";
+import HomeCards from "../HomeCards";
+import JobListings from "../JobListings";
+import ViewAllJobs from "../ViewAllJobs";
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <HomeCards />
+      <JobListings isHome={true} />
+      <ViewAllJobs />
+    </>
+  );
+};
+export default HomePage;
